09d163ddd712d472ed6db2ace33ffee473b94b4f
unknown <thericardoguilherme@gmail.com>
2023-01-30 20:29:38 -0300

    javacomhtml

---
+++ script.js
@@ -0,0 +1,14 @@
+            //mudar o texto do HTML atraves do JS
+
+    //const hora = document.getElementById("hora");
+    //hora.innerText = "Boa noite";
+    //hora.style = "color:red"
+
+
+
+
+function acenderDesligar(){
+    const luz = document.getElementById("luz");
+    luz.classList.toggle("ligada");
+}
+
